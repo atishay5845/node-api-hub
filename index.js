11d9e4d429156fe2716ee4f2e7952a72ec5ff9c1
atishay5845 <atishaysodhiya5845@gmail.com>
2026-04-19 11:18:45 +0530

    import and export es6 module system

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-require('./sum');
+// require('./sum');
 
 var a = 10;
 var b = 20;
 
-const obj = require('./sum');
-obj.calculateSum(a, b);
+// const obj = require('./sum');
+// obj.calculateSum(a, b);
+// console.log(obj.x);
 
-console.log(obj.x);
+
+//obj desturcturing
+// const { calculateSum, x } = require('./sum');
+// calculateSum(a, b);
+
+//es6 module system
+import { calculateSum, x } from './sum.js';
+calculateSum(a, b);
+
+console.log(x);
