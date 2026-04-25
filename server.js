16f0841f968bcc302325b5c628b3616ce50c2db0
atishay5845 <atishaysodhiya5845@gmail.com>
2026-04-25 23:45:33 +0530

    created server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require("http");
 
 const server = http.createServer(function (req,res){
-
+  if(req.url==="/getdata"){
+    res.end("ma kaa bhosda lawde");
+  }
   res.end("hello chutiye");//sending the data to user;
 });
 
